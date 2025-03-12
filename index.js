d770c05def5cec75edb7f47ec81c78c3ef096558
Michael Ouma <Oumamichael108@gmail.com>
2025-03-12 19:35:37 +0300

    filter() Drivers

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+const drivers = ["Jacob Aliet", "Verah", "Silas Nyanchwani", "Amerix", "Jadwar", "Michael Ouma"];
+function findMatching(drivers, string) {
+    return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+};
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(driver => driver.toLowerCase().indexOf(string.toLowerCase()) === 0);
+};
+function matchName(drivers, string) {
+    return drivers.filter(driver => driver.name === string);
+}; 
+const driverObjects = [
+    { name: "jacob aliet", hometown: "Naivasha" },
+    { name: "Silas Nyanchwani", hometown: "Kisumu" },
+    { name: "Sally", hometown: "Philadephia" },
+    { name: "Amerix", hometown: "Kakamega" },
+    { name: "Jadwar", hometown: "South-C" },
+    { name: "Jacob Aliet", hometown: "Nairobi" }
+];
+console.log(findMatching(drivers, "jacob aliet"));
+console.log(fuzzyMatch(drivers, "j"));
+console.log(matchName(driverObjects, "Jacob Aliet")); //case-sensitive
